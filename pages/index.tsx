3de16e4f9edde5514abdb1ef21d4570a13f362a4
Jason Alvarado <alvaradojasondev@gmail.com>
2022-06-12 16:55:26 -0300

    feat: clean unnecessary pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,14 @@
-import { Container } from "@material-ui/core";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
-const Home: NextPage = ({ products }: { products: any[] }) => {
+const Home: NextPage = () => {
   return (
     <div>
-      <Container>
-        <section>
-          {products &&
-            products.map(({ _id: id, name, img, cost, category }) => (
-              <article key={id}>
-                <h1>{name}</h1>
-                <p>{cost}</p>
-                <Image src={img.url} width="200" height="200" alt={name} />
-              </article>
-            ))}
-        </section>
-      </Container>
+      <Layout>
+        <h1>Index</h1>
+      </Layout>
     </div>
   );
 };
-
-export async function getStaticProps() {
-  try {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    headers.append(
-      "Authorization",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmE1NTNhYjBjZGUzODAwMjFlYzczMGEiLCJpYXQiOjE2NTUwMDIwMjd9.hpH-0Ndnx4kbM704_BcmQZAkdKZL1HredoaAVbmJR7U"
-    ); //read from .env
-
-    const prodsUrl =
-      "https://private-anon-b24e8508cf-aerolabchallenge.apiary-proxy.com/products";
-
-    const products = await fetch(prodsUrl, {
-      method: "GET",
-      headers: headers,
-    })
-      .then((data) => (data.ok ? data.json() : null))
-      .catch((err) => console.error(err));
-
-    return {
-      props: {
-        products,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 export default Home;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,12 +1,55 @@
+import { Container } from "@material-ui/core";
+import Image from "next/image";
 import React from "react";
 import Layout from "../../components/Layout";
 
-const index = () => {
+const index = ({ products }: { products: any[] }) => {
   return (
     <Layout title="Products" description="Products page">
-      <h1>Products</h1>
+      <Container>
+        <h1>Products</h1>
+        <section>
+          {products &&
+            products.map(({ _id: id, name, img, cost, category }) => (
+              <article key={id}>
+                <h1>{name}</h1>
+                <p>{cost}</p>
+                <Image src={img.url} width="200" height="200" alt={name} />
+              </article>
+            ))}
+        </section>
+      </Container>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  try {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append(
+      "Authorization",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmE1NTNhYjBjZGUzODAwMjFlYzczMGEiLCJpYXQiOjE2NTUwMDIwMjd9.hpH-0Ndnx4kbM704_BcmQZAkdKZL1HredoaAVbmJR7U"
+    ); //read from .env
+
+    const prodsUrl =
+      "https://private-anon-b24e8508cf-aerolabchallenge.apiary-proxy.com/products";
+
+    const products = await fetch(prodsUrl, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((data) => (data.ok ? data.json() : null))
+      .catch((err) => console.error(err));
+
+    return {
+      props: {
+        products,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default index;
