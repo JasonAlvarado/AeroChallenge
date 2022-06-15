06e85d188647ad8f63bf1740a5da38006c712caa
Jason Alvarado <alvaradojasondev@gmail.com>
2022-06-15 08:15:21 -0300

    feat: landing page first design

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -1,18 +1,27 @@
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>EXPLORE THE TECH ZONE</h1>
-      <p>
-        Here you&apos;ll be able to exchange all of your hard-earned Aeropoints
-        and exchange them for cool tech.
-      </p>
-      <Button variant="contained">
-        <a href="#products">VIEW ALL PRODUCTS</a>
-      </Button>
-    </div>
+    <Grid container spacing={4}>
+      <Grid item lg={6} style={{ textAlign: "center" }}>
+        <p className="banner-secondary">EXPLORE THE</p>
+        <div className="banner">
+          <span className="text-grad">TECH</span>
+          <span style={{ color: "#252F3D" }}>ZONE</span>
+        </div>
+        <p className="banner-secondary">
+          Here you&apos;ll be able to exchange all of your hard-earned
+          Aeropoints and exchange them for cool tech.
+        </p>
+        <button className="btn-grad">
+          <a href="#products">VIEW ALL PRODUCTS ⬇</a>
+        </button>
+      </Grid>
+      <Grid item lg={6}>
+        <img src="/img/banner.png" alt="banner" width={"100%"} />
+      </Grid>
+    </Grid>
   );
 };
 
