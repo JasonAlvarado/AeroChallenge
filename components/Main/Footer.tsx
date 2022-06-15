a419b62dc7b48be159bbcc5aad93f26e4cded849
Jason Alvarado <alvaradojasondev@gmail.com>
2022-06-15 08:16:21 -0300

    feat: footer update

--- components/Main/Footer.tsx
+++ components/Main/Footer.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div style={{ textAlign: "center", margin: "40px 0 20px 0" }}>
-      <a href="#">View this repository</a>
-    </div>
+    <footer style={{ textAlign: "center", margin: "40px 0 20px 0" }}>
+      <a href="https://github.com/JasonAlvarado/AeroChallenge.git">
+        {/* <Image
+          src="/img/github-default.svg"
+          height={24}
+          width={24}
+          style={{ display: "inline-block", margin: "0 20px" }}
+        ></Image> */}
+        <p style={{ display: "inline-block", fontSize: "1.2em" }}>
+          View this repository
+        </p>
+      </a>
+    </footer>
   );
 };
 
