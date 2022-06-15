ba0df0adb231bdbd52eff1ca85a9d394abf2aa9d
Jason Alvarado <alvaradojasondev@gmail.com>
2022-06-15 08:16:02 -0300

    feat: header update

--- components/Main/Header.tsx
+++ components/Main/Header.tsx
@@ -1,11 +1,12 @@
 import { Container } from "@material-ui/core";
 import React from "react";
+import UserCard from "../User/UserCard";
 
 const Header = () => {
   return (
     <Container>
-      <h1>aerolab</h1>
-      <p>10.000</p>
+      {/* <h1>aerolab</h1> */}
+      <UserCard />
     </Container>
   );
 };
